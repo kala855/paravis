32098a4c9e2770c8217408492380934d9261e927
John Osorio <john@sirius.utp.edu.co>
2016-07-06 12:05:34 -0500

    Se adiciona código necesario para que se pueda compilar con Paraview y se crea un código de ejemplo basico

---
+++ src/atriumModel/visual/paravisAdaptor.cxx
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "paravisAdaptor.h"
+
+#include <vtkCPDataDescription.h>
+#include <vtkCPInputDataDescription.h>
+#include <vtkCPProcessor.h>
+#include <vtkCPPythonScriptPipeline.h>
+#include <vtkDoubleArray.h>
+#include <vtkFloatArray.h>
+#include <vtkImageData.h>
+#include <vtkNew.h>
+#include <vtkPoints.h>
+#include <vtkPointData.h>
+
+
+vtkCPProcessor* Processor = NULL;
+
+namespace paravisAdaptor{
+    void Initialize(int numScripts, char* scripts[]){
+        if(Processor == NULL){
+            Processor = vtkCPProcessor::New();
+            Processor->Initialize();
+        }
+
+        for (int i = 1; i < numScripts; i++) {
+            vtkCPPythonScriptPipeline* pipeline = vtkCPPythonScriptPipeline::New();
+            pipeline->Initialize(scripts[i]);
+            Processor->AddPipeline(pipeline);
+            pipeline->Delete();
+        }
+    }
+
+    void Finalize(){
+        if(Processor){
+            Processor->Delete();
+            Processor = NULL;
+        }
+    }
+
+
+    void CoProcess(int timeStep, double time, unsigned int numPoints[3],
+            double spacing[3], double *potential){
+
+        vtkCPDataDescription *dataDescription = vtkCPDataDescription::New();
+        dataDescription->AddInput("input");
+        dataDescription->SetTimeData(time,timeStep);
+
+        if(Processor->RequestDataDescription(dataDescription) != 0){
+            //Catalyst needs to output data
+            //Create an axis-aligned, uniform grid
+            vtkImageData* grid = vtkImageData::New();
+            grid->SetExtent(0,numPoints[0]-1, 0, numPoints[1]-1, 0, 0);
+            dataDescription->GetInputDescriptionByName("input")->SetGrid(grid);
+            grid->Delete();
+            //Create a  potential associated with points
+            vtkDoubleArray* array = vtkDoubleArray::New();
+            array->SetName("Potential");
+            array->SetArray(potential, grid->GetNumberOfPoints(),1);
+            grid->GetPointData()->AddArray(array);
+            array->Delete();
+            Processor->CoProcess(dataDescription);
+        }
+        dataDescription->Delete();
+    }
+}
--- src/atriumModel/visual/paravisAdaptor.h
+++ src/atriumModel/visual/paravisAdaptor.h
@@ -1,53 +1,11 @@
-#include <stdio.h>
 
-void CatalystInit(int numScripts, char * scripts[])
+
+namespace paravisAdaptor
 {
-    if (Processor == NULL){
-        Processor = vtkCPProcessor::New();
-        Processor->Initialize();
-    }
-    // Scripts are Passed in as command line arguments
-    for (int i = 0; i < numScripts; i++) {
-        vtkCPPythonScriptPipeline* pipeline = vtkCPPythonScriptPipeline::New();
-        pipeline->Initialize(scripts[i]);
-        Processor->AddPipeline(pipeline);
-        pipeline->Delete();
-    }
+    void Initialize(int numScripts, char* scripts[]);
 
-}
+    void Finalize();
 
-
-void CatalystFinalize(){
-    if(Processor){
-        Processor->Delete();
-        Processor = NULL;
-    }
-}
-
-
-//The grid is a uniform, rectilinear grid that can be specified
-//with the number of points in each direction and the uniform
-//spacing between points. There is only one fiel called
-//potential which is specified over the points/nodes of the grid
-void CatalysCoProcess(int timeStep, double time, unsigned int numPoints[3], double spacing[3], double *field){
-    vtkCPDataDescription* dataDescription = vtkCPDataDescription::New();
-    dataDescription->AddInput("input");
-    dataDescription->SetTimeData(time, timeStep);
-
-    if(Processor->RequestDataDescription(dataDescription)!=0){
-        // Catalyst needs to output data
-        // Create an axis-aligned, uniform grid
-        vtkImageData* grid = vtkImageData::New();
-        grid->SetExtents(0,numPoints[0]-1, 0, numPoints[1]-1,0, numPoints[2]-1);
-        dataDescription->GetInputDescriptionByName("input")->SetGrid(grid);
-        grid->Delete();
-        // Create a field associated with points
-        vtkDoubleArray* array = vtkDoubleArray::New();
-        array->SetName("potential");
-        array->SetArray(field, grid->GetNumberOfPoints(),1);
-        grid->GetPointData()->AddArray(array);
-        array->Delete();
-        Processor->CoProcess(dataDescription);
-    }
-    dataDescription->Delete();
+    void CoProcess(int timeStep, double time, unsigned int numPoints[3],
+            double spacing[3], double *potential);
 }
