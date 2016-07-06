e7f3f56f2a6b7ee7196d86329a65c14b13e9d8bb
John Osorio <john@sirius.utp.edu.co>
2016-07-06 15:02:53 -0500

    PAR-6 #time 10h #finish

--- src/atriumModel/visual/visual.cxx
+++ src/atriumModel/visual/visual.cxx
@@ -39,19 +39,13 @@ int main(int argc, char* argv[]){
     numPoints[0] = 150;
     numPoints[1] = 150;
     numPoints[2] = 0;
-//#ifdef CATALYST
     Initialize(argc, argv);
-//#endif
     for (int timeStep=0; timeStep<numberOfTimeSteps;timeStep++)
     {
         updateData(data, sizeData);
         delay(1000);
-//#ifdef CATALYST
         CoProcess(timeStep,timeStep*0.1,numPoints,spacing,data);
-//#endif
 
     }
-//#ifdef CATALYST
     Finalize();
-//#endif
 }
